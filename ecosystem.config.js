328dd9e6b41a03a5d6815f0e1e4816e4bc5c259a
akashk447 <akashk447@gmail.com>
2024-09-26 13:35:54 +0530

    fasdfas

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    apps: [
+      {
+        name: 'vite-app',
+        script: 'npm',
+        args: 'run dev',
+        instances: 1,
+        autorestart: true,
+        watch: false,
+        max_memory_restart: '1G',
+        env: {
+          NODE_ENV: 'development',
+        },
+      },
+    ],
+  };
